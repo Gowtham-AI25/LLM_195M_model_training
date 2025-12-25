5613baa2ba817ca3c8e28f39ff470924037c311f
GOLI OMKAR MYTHILI GOWTHAM <164644498+Gowtham-AI25@users.noreply.github.com>
2025-12-25 13:36:05 +0530

    Update train.py

--- train.py
+++ train.py
@@ -1,8 +1,5 @@
 import os
-# os.environ["HF_HUB_DISABLE_PROGRESS_BARS"] = "1"
-# os.environ["TORCHINDUCTOR_CUDAGRAPHS"] = "0"
-# import torch._inductor.config as inductor_config
-# inductor_config.triton.cudagraphs = False
+os.environ["HF_HUB_DISABLE_PROGRESS_BARS"] = "1"
 import torch 
 import gc
 import wandb
@@ -184,6 +181,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
 
